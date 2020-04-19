fc541e3dc14ada9789976549007a2e7bb349bd81
Fernando <fer.solerv@hotmail.com>
2020-04-19 11:32:07 +0200

    commit

---
+++ build/husky_development/husky_vel/CMakeFiles/husky_robot.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/parallels/catkin_ws/src/husky_development/husky_vel/src/husky_robot.cpp" "/home/parallels/catkin_ws/build/husky_development/husky_vel/CMakeFiles/husky_robot.dir/src/husky_robot.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"husky_development\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/ros/melodic/include"
+  "/opt/ros/melodic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  "/usr/include/include"
+  "/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/husky_development/scripts/tf_prientation_tb3_robot.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import rospy
-from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
-import math
-import time
-from std_srvs.srv import Empty
-import tf
